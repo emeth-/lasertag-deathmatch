98fe4544549c28d4e37800b6e1f088e5d9126433
Emeth <seanybob@gmail.com>
2015-10-31 06:41:54 -0500

    oops

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -89,7 +89,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
